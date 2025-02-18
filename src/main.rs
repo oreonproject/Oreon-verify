b00a5a9404083b59669b1a1e3245c69659f33d4c
Ali moumen <moumenisbest@gmail.com>
2025-02-18 19:17:40 +0100

    Added ISO checking functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use clap::Parser;
+mod checker;
 #[derive(Parser, Debug)]
 #[command[version = "1.0", long_about = None, about = "Verify the integrity of ISO files."]]
 struct Args {
@@ -7,23 +8,20 @@ struct Args {
     file_name: Option<String>,
     #[arg(short = 'H', long = "hash")]
     hash: Option<String>,
-    #[arg(short = 'v', long = "verbose")]
-    verbose: bool
 }
 fn main() {
     let args = Args::parse();
-    if args.verbose {
-        println!("[INFO] Validating arguments.");
-    }
+    println!("[INFO] Validating arguments.");
     match validate_args(&args) {
         Ok(()) => {
-            if args.verbose {
-                println!("[INFO] Arguments seem correct.");
-            }
+            println!("[INFO] Arguments seem correct.");
         }
         Err(msg) => panic!("[ERR] {msg}")
     };
-
+    match checker::check(&args.file_name.unwrap(), &args.hash.unwrap()) {
+        true => println!("ISO file is fine!"),
+        false => println!("ISO file is corrupted!")
+    }
 }
 
 fn validate_args(args: &Args) -> Result<(), &str> {
